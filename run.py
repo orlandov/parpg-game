b5e5eb9d6bd4d0bc02ecccbe50d593161e24e269
maximinus_parpg <maximinus_parpg@2475d057-1fa1-4ed2-bda5-76bff2e61f20>
2009-04-08 02:06:16 +0000

    Added new transitions layer to map Converted all code in /demo/ to use spaces, not tabs
    git-svn-id: http://parpg-svn.cvsdude.com/parpg/trunk/demo@30 2475d057-1fa1-4ed2-bda5-76bff2e61f20

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python
 
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
 
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys, os, shutil
 
@@ -34,86 +34,86 @@ from settings import Setting
 TDS = Setting()
 
 class ApplicationListener(eventlistenerbase.EventListenerBase):
-	def __init__(self, engine, world):
-		super(ApplicationListener, self).__init__(engine,
-			regKeys=True,regCmd=True, regMouse=False, 
-			regConsole=True, regWidget=True)
-		self.engine = engine
-		self.world = world
-		engine.getEventManager().setNonConsumableKeys([fife.Key.ESCAPE,])
-		self.quit = False
-		self.aboutWindow = None
+    def __init__(self, engine, world):
+        super(ApplicationListener, self).__init__(engine,
+            regKeys=True,regCmd=True, regMouse=False, 
+            regConsole=True, regWidget=True)
+        self.engine = engine
+        self.world = world
+        engine.getEventManager().setNonConsumableKeys([fife.Key.ESCAPE,])
+        self.quit = False
+        self.aboutWindow = None
 
-	def keyPressed(self, evt):
-		"""Function to deal with keypress events
+    def keyPressed(self, evt):
+        """Function to deal with keypress events
           
-		   @param evt: The event that was captured"""
-		keyval = evt.getKey().getValue()
-		if keyval == fife.Key.ESCAPE:
-			self.quit = True
-		evt.consume()
+           @param evt: The event that was captured"""
+        keyval = evt.getKey().getValue()
+        if keyval == fife.Key.ESCAPE:
+            self.quit = True
+        evt.consume()
 
 class PARPG(ApplicationBase):
-	"""Main Application class"""
-	def __init__(self):
-		super(PARPG,self).__init__()
-		self.world = world.World(self.engine)
-		self.listener = ApplicationListener(self.engine, self.world)
-		self.world.load(str(TDS.readSetting("MapFile")))
+    """Main Application class"""
+    def __init__(self):
+        super(PARPG,self).__init__()
+        self.world = world.World(self.engine)
+        self.listener = ApplicationListener(self.engine, self.world)
+        self.world.load(str(TDS.readSetting("MapFile")))
 
-	def loadSettings(self):
-		"""Load the settings from a python file and load them into the engine.
-		   Called in the ApplicationBase constructor."""
-		import settings
-		self.settings = settings
+    def loadSettings(self):
+        """Load the settings from a python file and load them into the engine.
+           Called in the ApplicationBase constructor."""
+        import settings
+        self.settings = settings
 
-		eSet = self.engine.getSettings()
-		eSet.setDefaultFontGlyphs(str(
-		TDS.readSetting("FontGlyphs",strip=False)))
-		eSet.setDefaultFontPath(str(TDS.readSetting("Font")))
-		eSet.setBitsPerPixel(int(TDS.readSetting("BitsPerPixel")))
-		eSet.setInitialVolume(float(TDS.readSetting("InitialVolume")))
-		eSet.setSDLRemoveFakeAlpha(int(TDS.readSetting("SDLRemoveFakeAlpha")))
-		eSet.setScreenWidth(int(TDS.readSetting("ScreenWidth")))
-		eSet.setScreenHeight(int(TDS.readSetting("ScreenHeight")))
-		eSet.setRenderBackend(str(TDS.readSetting("RenderBackend")))
-		eSet.setFullScreen(int(TDS.readSetting("FullScreen")))
-		try:
-			eSet.setWindowTitle(str(TDS.readSetting("WindowTitle")))
-			eSet.setWindowIcon(str(TDS.readSetting("WindowIcon")))
-		except:
-			pass			
-		try:
-			eSet.setImageChunkingSize(int(TDS.readSetting("ImageChunkSize")))
-		except:
-			pass
+        eSet = self.engine.getSettings()
+        eSet.setDefaultFontGlyphs(str(
+        TDS.readSetting("FontGlyphs",strip=False)))
+        eSet.setDefaultFontPath(str(TDS.readSetting("Font")))
+        eSet.setBitsPerPixel(int(TDS.readSetting("BitsPerPixel")))
+        eSet.setInitialVolume(float(TDS.readSetting("InitialVolume")))
+        eSet.setSDLRemoveFakeAlpha(int(TDS.readSetting("SDLRemoveFakeAlpha")))
+        eSet.setScreenWidth(int(TDS.readSetting("ScreenWidth")))
+        eSet.setScreenHeight(int(TDS.readSetting("ScreenHeight")))
+        eSet.setRenderBackend(str(TDS.readSetting("RenderBackend")))
+        eSet.setFullScreen(int(TDS.readSetting("FullScreen")))
+        try:
+            eSet.setWindowTitle(str(TDS.readSetting("WindowTitle")))
+            eSet.setWindowIcon(str(TDS.readSetting("WindowIcon")))
+        except:
+            pass            
+        try:
+            eSet.setImageChunkingSize(int(TDS.readSetting("ImageChunkSize")))
+        except:
+            pass
 
-	def initLogging(self):
-		"""Initialize the LogManager"""
-		LogModules = TDS.readSetting("LogModules",type='list')
-		self.log = fifelog.LogManager(self.engine,
-		int(TDS.readSetting("LogToPrompt")),
-		int(TDS.readSetting("LogToFile")))
-		if LogModules:
-			self.log.setVisibleModules(*LogModules)
+    def initLogging(self):
+        """Initialize the LogManager"""
+        LogModules = TDS.readSetting("LogModules",type='list')
+        self.log = fifelog.LogManager(self.engine,
+        int(TDS.readSetting("LogToPrompt")),
+        int(TDS.readSetting("LogToFile")))
+        if LogModules:
+            self.log.setVisibleModules(*LogModules)
 
-	def createListener(self):
-		# already created in constructor
-		pass
+    def createListener(self):
+        # already created in constructor
+        pass
 
-	def _pump(self):
-		"""Main game loop
-		   There are in fact 2 main loops, this one and the one in World"""
-		if self.listener.quit:
-			self.breakRequested = True
-		else:
-			self.world.pump()
+    def _pump(self):
+        """Main game loop
+           There are in fact 2 main loops, this one and the one in World"""
+        if self.listener.quit:
+            self.breakRequested = True
+        else:
+            self.world.pump()
 
 def main():
-	"""Application code starts from here"""
-	app = PARPG()
-	app.run()
+    """Application code starts from here"""
+    app = PARPG()
+    app.run()
 
 if __name__ == '__main__':
-	main()
+    main()
 
--- settings.py
+++ settings.py
@@ -1,52 +1,52 @@
 import shutil
 
 try:
-	import xml.etree.cElementTree as ET
+    import xml.etree.cElementTree as ET
 except:
-	import xml.etree.ElementTree as ET
+    import xml.etree.ElementTree as ET
 
 class Setting(object):
-	def setDefaults(self):
-		shutil.copyfile('settings-dist.xml', 'settings.xml')
-		self.isSetToDefault = True
-		self.changesRequireRestart = True
+    def setDefaults(self):
+        shutil.copyfile('settings-dist.xml', 'settings.xml')
+        self.isSetToDefault = True
+        self.changesRequireRestart = True
 
-	def readSetting(self, name, type='int', strip=True, text=False):
-		if not hasattr(self, 'tree'):
-			self.tree = ET.parse('settings.xml')
-			self.root_element = self.tree.getroot()
-		element = self.root_element.find(name)
-		if element is not None:
-			element_value = element.text
-			if element_value is None:
-				if type == 'int':
-					return 0
-				elif type == 'list':
-					list = []
-					return list
-			else:
-				if type == 'int':
-					return element_value.strip() if strip else element_value
-				elif type == 'list':
-					list = []
-					list_s = []
-					list = str(element_value.strip()).split(";")
-					for item in list:
-						item = item.strip()
-						if text:
-							item = item.replace('\\n', '\n')
-						list_s.append(item)
-					return list_s
-				elif type == 'bool':
-					return False if element_value.strip() == 'False' else True
-		else:
-			print 'Setting,', name, 'does not exist!'
+    def readSetting(self, name, type='int', strip=True, text=False):
+        if not hasattr(self, 'tree'):
+            self.tree = ET.parse('settings.xml')
+            self.root_element = self.tree.getroot()
+        element = self.root_element.find(name)
+        if element is not None:
+            element_value = element.text
+            if element_value is None:
+                if type == 'int':
+                    return 0
+                elif type == 'list':
+                    list = []
+                    return list
+            else:
+                if type == 'int':
+                    return element_value.strip() if strip else element_value
+                elif type == 'list':
+                    list = []
+                    list_s = []
+                    list = str(element_value.strip()).split(";")
+                    for item in list:
+                        item = item.strip()
+                        if text:
+                            item = item.replace('\\n', '\n')
+                        list_s.append(item)
+                    return list_s
+                elif type == 'bool':
+                    return False if element_value.strip() == 'False' else True
+        else:
+            print 'Setting,', name, 'does not exist!'
 
-	def setSetting(self, name, value):
-		element = self.root_element.find(name)
-		if element is not None:
-			if value is not element.text:
-				element.text = str(value)
-		else:
-			print 'Setting,', name, 'does not exist!'
+    def setSetting(self, name, value):
+        element = self.root_element.find(name)
+        if element is not None:
+            if value is not element.text:
+                element.text = str(value)
+        else:
+            print 'Setting,', name, 'does not exist!'
 
