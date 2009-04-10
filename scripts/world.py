c057f876d0121705e0acf0c1668c29d182a8bf04
maximinus_parpg <maximinus_parpg@2475d057-1fa1-4ed2-bda5-76bff2e61f20>
2009-04-10 13:34:20 +0000

    Minor edit to world.py license (changed foobar to PARPG) First rough draft of tile transition layer utility added
    git-svn-id: http://parpg-svn.cvsdude.com/parpg/trunk/demo@35 2475d057-1fa1-4ed2-bda5-76bff2e61f20

--- scripts/world.py
+++ scripts/world.py
@@ -13,7 +13,7 @@
 #   GNU General Public License for more details.
 
 #   You should have received a copy of the GNU General Public License
-#   along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#   along with PARPG.  If not, see <http://www.gnu.org/licenses/>.
 
 import fife
 from scripts.common.eventlistenerbase import EventListenerBase
