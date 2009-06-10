259995a2310eae1baf91b4ebe285f18ec1504ea3
maximinus_parpg <maximinus_parpg@2475d057-1fa1-4ed2-bda5-76bff2e61f20>
2009-06-10 16:35:54 +0000

    Added meggies code to move the NPC Good work!

--- scripts/agents/npc.py
+++ scripts/agents/npc.py
@@ -15,11 +15,86 @@
 #   You should have received a copy of the GNU General Public License
 #   along with PARPG.  If not, see <http://www.gnu.org/licenses/>.
 
-class NPC:
+import fife
+from settings import Setting
+from random import randint
+
+TDS = Setting()
+_STATE_NONE, _STATE_IDLE, _STATE_WANDER = xrange(3)
+
+class NPC(fife.InstanceActionListener):
     """This is the class we use for all NPCs"""
-    def __init__(self, xpos, ypos, ident, text):
-        self.xpos = xpos
-        self.ypos = ypos
-        self.id = ident
+    def __init__(self, text, agentName, layer):
+        """Init function.
+           @param text: a string of text that will be output to screen when
+               character is right clicked
+           @param agentName: the 'id' of the NPC in the map_object.xml file
+           @param layer: a fife.Instance object, (engine.view.agent_layer)"""
+        fife.InstanceActionListener.__init__(self)
         self.text = text
+        self.agentName = agentName
+        # a fife.Instance
+        self.agent = layer.getInstance(agentName)
+        self.agent.addActionListener(self)
+        self.state = _STATE_NONE
+        self.speed = float(TDS.readSetting("PCSpeed"))-1
+        self.target_loc = self.getTargetLocation() 
+        self.text = text
+
+    def getX(self):
+        """@return: the x coordinate of the NPC's location as an int"""
+        return self.agent.getLocation().getLayerCoordinates().x
+
+    def getY(self):
+        """@return: the y coordinate of the NPC's location as an int"""
+        return self.agent.getLocation().getLayerCoordinates().y
+
+    def getTargetLocation(self):
+        """@return: a fife.Location object based off of the NPC's
+           position and current state"""
+        x = self.getX()
+        y = self.getY()
+        if self.state == _STATE_WANDER:
+            """ Random Walk """
+            rl = randint(-1, 1)
+            ud = randint(-1, 1)
+            x += rl
+            y += ud
+        l = fife.Location(self.agent.getLocation())
+        l.setLayerCoordinates(fife.ModelCoordinate(*tuple([x, y])))
+        return l
+    
+    def onInstanceActionFinished(self, instance, action):
+        """What the NPC does when it has finished an action.
+           Called somewhere else (TODO: where?)"""
+        if self.state == _STATE_WANDER:
+            self.target_loc = self.getTargetLocation()
+        self.idle()
+
+    def start(self):
+        self.idle()
+
+    def idle(self):
+        """Controls the NPC when it is idling. Different actions
+           based on the NPC's state"""
+        if self.state == _STATE_NONE:
+            self.state = _STATE_IDLE
+            self.agent.act('stand', self.agent.getFacingLocation())
+        elif self.state == _STATE_IDLE:
+            self.target_loc = self.getTargetLocation()
+            self.state = _STATE_WANDER
+            self.agent.act('stand', self.agent.getFacingLocation())
+        elif self.state == _STATE_WANDER:
+            self.wander(self.target_loc)
+
+    def wander(self, location):
+        """Nice slow movement for random walking.
+           @param location: a fife.Location object, where the NPC
+           will walk to"""
+        self.agent.move('walk', location, self.speed-1)
+
+    def run(self, location):
+        """Faster movement than walk.
+           @param location: a fife.Location object, where the NPC will run to"""
+        self.agent.move('run', location, self.speed+1)
 
