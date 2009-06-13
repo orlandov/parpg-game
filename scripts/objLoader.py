013cb586565d0230bd07d17f626a60fb286df2a8
maximinus_parpg <maximinus_parpg@2475d057-1fa1-4ed2-bda5-76bff2e61f20>
2009-06-13 07:43:28 +0000

    Added new functionality. Right-click on a door to go to a new map.
    git-svn-id: http://parpg-svn.cvsdude.com/parpg/trunk/PARPG@130 2475d057-1fa1-4ed2-bda5-76bff2e61f20

--- scripts/objLoader.py
+++ scripts/objLoader.py
@@ -27,12 +27,38 @@ class LocalXMLParser(ContentHandler):
         self.pc = None
         self.objects = []
         self.npcs = []
+        self.doors = []
         self.tele_tiles = []
     
     def getParser(self):
         """Simple one liner to remove XML dependencies in engine.py"""
         return make_parser()
     
+    def getObject(self, attrs):
+        """Grab the object details from the XML"""
+        try:
+            display = attrs.getValue("display")
+            if(display == "True"):
+                xpos = attrs.getValue("xpos")
+                ypos = attrs.getValue("ypos")
+            else:
+                owner = attrs.getValue("owner")
+            gfx = attrs.getValue("gfx")
+            ident = attrs.getValue("id")
+            text = attrs.getValue("text")
+            contain = attrs.getValue("contain")
+            carry = attrs.getValue("carry")
+        except(KeyError):
+            sys.stderr.write("Error: Data missing in object definition\n")
+            sys.exit(False)
+        # now we have the data, save it for later
+        if(display == "True"):
+            self.objects.append([True, xpos, ypos, gfx, ident,
+                                 text, contain, carry])
+        else:
+            self.objects.append([False, gfx, ident, text,
+                                 owner, contain, carry])
+  
     def startElement(self, name, attrs):
         """Called every time we meet a new element in the XML file"""
         # we are only looking for the 'layer' elements, the rest we ignore
@@ -65,29 +91,19 @@ class LocalXMLParser(ContentHandler):
             self.npcs.append([xpos, ypos, gfx, ident, text])
         elif(name == "object"):
             # same old same old
+            self.getObject(attrs)
+        elif(name == "door"):
+            # firstly, add the object
+            self.getObject(attrs)
+            # then save the other data
             try:
-                display = attrs.getValue("display")
-                if(display == "True"):
-                    xpos = attrs.getValue("xpos")
-                    ypos = attrs.getValue("ypos")
-                else:
-                    owner = attrs.getValue("owner")
-                gfx = attrs.getValue("gfx")
-                ident = attrs.getValue("id")
-                text = attrs.getValue("text")
-                contain = attrs.getValue("contain")
-                carry = attrs.getValue("carry")
+                new_map = attrs.getValue("map")
             except(KeyError):
-                sys.stderr.write("Error: Data missing in object definition\n")
+                sys.stderr.write("Error: Door has no map!\n")
                 sys.exit(False)
-            # now we have the data, save it for later
-            if(display == "True"):
-                self.objects.append([True, xpos, ypos, gfx, ident,
-                                     text, contain, carry])
-            else:
-                self.objects.append([False, gfx, ident, text,
-                                     owner, contain, carry])
-        elif name == "tele_tile":
+            # format is [id,map_name]
+            self.doors.append([self.objects[-1][4],new_map])
+        elif(name == "tele_tile"):
             try:
                 target = attrs.getValue("target")
                 xpos = attrs.getValue("xpos")
@@ -96,7 +112,7 @@ class LocalXMLParser(ContentHandler):
                 typos = attrs.getValue("typos")
             except(KeyError):
                 sys.stderr.write("Error: Data missing in \
-                        tele_tile definition\n")
+                                  tele_tile definition\n")
                 sys.exit(False)
             self.tele_tiles.append([target, tuple([int(xpos), int(ypos)]), \
                     tuple([int(txpos), int(typos)])])
