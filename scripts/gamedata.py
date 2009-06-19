edd4f86857e4582a5dafa4eed78d70d2602ee1ce
maximinus_parpg <maximinus_parpg@2475d057-1fa1-4ed2-bda5-76bff2e61f20>
2009-06-19 13:41:49 +0000

    Merged gamestate and engine. Fixed a few typos.

--- scripts/gamedata.py
+++ scripts/gamedata.py
@@ -15,61 +15,57 @@
 #   You should have received a copy of the GNU General Public License
 #   along with PARPG.  If not, see <http://www.gnu.org/licenses/>.
 
-import fife
-
 class BaseObjectData(object):
     """Basis for game object data.
     """
-    def __init__(self, id, posx, posy, text, map, gfx=None, display=True):
+    def __init__(self, ident, posx, posy, text, cmap, gfx=None, display=True):
         """Set the basic values that are shared by all game objects.
-        @type id: String
-        @param id: Unique identifier for this object.
+        @type ident: String
+        @param ident: Unique identifier for this object.
         @type posx: Integer
         @param posx: Position X
         @type posy: Integer
         @param posy: Position Y
         @type text: String
         @param text: The caption of this object
-        @type map: String
-        @param map: Map where this object is currently located
+        @type cmap: String
+        @param cmap: Map where this object is currently located
         @type display: Bool
         @param display: Display this object?
         @type gfx: String
         @param gfx: ???
-        @returns: None
-        """
-        self.id = id
+        @returns: None"""
+        self.id = ident
         self.posx = posx
         self.posy = posy
         self.display = display
         self.text = text
-        self.map = map
+        self.map = cmap
         self.gfx = gfx
     
     def __cmp__(self, other):
         """Compares this instance to another.
-        @type other: baseObject
-        @param other: the other object to compare
-        @returns: True if the ids are equal, false otherwise.
-        """
+        @type other: BaseObjectData
+        @param other: The other object to compare
+        @returns: True if the ids are equal, false otherwise."""
         return self.id == other.id
     
 class DoorData(BaseObjectData):
     """Represents a door.
     """
-    def __init__(self, id, posx, posy, text, map, gfx, display,
+    def __init__(self, ident, posx, posy, text, cmap, gfx, display,
                  owner, destx, desty, destmap):
         """Set initial values
-        @type id: String
-        @param id: Unique identifier for this object.
+        @type ident: String
+        @param ident: Unique identifier for this object.
         @type posx: Integer
         @param posx: Position X
         @type posy: Integer
         @param posy: Position Y
         @type text: String
         @param text: The caption of this object
-        @type map: String
-        @param map: Map where this NPC is currently located
+        @type cmap: String
+        @param cmap: Map where this NPC is currently located
         @type display: Bool
         @param display: Display this object?
         @type gfx: String
@@ -83,7 +79,8 @@ class DoorData(BaseObjectData):
         @type destmap: String
         @param destmap: Target map
         @returns: None"""
-        super(DoorData, self).__init__(id, posx, posy, text, map, gfx, display)
+        super(DoorData, self).__init__(ident, posx, posy, text,
+                                       cmap, gfx, display)
         self.destx = destx
         self.desty = desty
         self.owner = owner
@@ -92,60 +89,62 @@ class DoorData(BaseObjectData):
 class NpcData(BaseObjectData):
     """Non Player Character
     """
-    def __init__(self, id, posx, posy, text, map, gfx=None, display=True):
+    def __init__(self, ident, posx, posy, text, cmap, gfx=None, display=True):
         """Set initial NPC values.
-        @type id: String
-        @param id: Unique identifier for this object.
+        @type ident: String
+        @param ident: Unique identifier for this object.
         @type posx: Integer
         @param posx: Position X
         @type posy: Integer
         @param posy: Position Y
         @type text: String
         @param text: The caption of this object
-        @type map: String
-        @param map: Map where this NPC is currently located
+        @type cmap: String
+        @param cmap: Map where this NPC is currently located
         @type display: Bool
         @param display: Display this object?
         @type gfx: String
         @param gfx: ???
         @returns: None"""
-        super(NpcData, self).__init__(id, posx, posy, text, map, gfx, display)
+        super(NpcData, self).__init__(ident, posx, posy, text,
+                                      cmap, gfx, display)
 
 class HeroData(NpcData):
     """Hero character, for now equal to a NPC"""
-    def __init__(self, id, posx, posy, text, map, gfx=None, display=True):
+    def __init__(self, ident, posx, posy, text, cmap, gfx=None, display=True):
         """Set initial NPC values.
-        @type id: String
-        @param id: Unique identifier for this object.
+        @type ident: String
+        @param ident: Unique identifier for this object.
         @type posx: Integer
         @param posx: Position X
         @type posy: Integer
         @param posy: Position Y
         @type text: String
         @param text: The caption of this object
-        @type map: String
-        @param map: Map where this NPC is currently located
+        @type cmap: String
+        @param cmap: Map where this NPC is currently located
         @type display: Bool
         @param display: Display this object?
         @type gfx: String
         @param gfx: ???
         @returns: None"""
-        super(HeroData, self).__init__(id, posx, posy, text, map, gfx, display)
+        super(HeroData, self).__init__(ident, posx, posy, text,
+                                       cmap, gfx, display)
 
 class NonLivingObjectData(BaseObjectData):
-    def __init__(self, id, posx, posy, text, map, gfx=None, display=True, 
+    def __init__(self, ident, posx, posy, text, cmap, gfx=None, display=True, 
                  owner=None, container=False, carryable=False):
         """Set initial object values.
-        @type id: String
-        @param id: Unique identifier for this object.
+        @type ident: String
+        @param ident: Unique identifier for this object.
         @type posx: Integer
         @param posx: Position X
         @type posy: Integer
         @param posy: Position Y
         @type text: String
         @param text: The caption of this object
-        @type map: String
-        @param map: Map where this NPC is currently located
+        @type cmap: String
+        @param cmap: Map where this NPC is currently located
         @type owner: ???
         @param owner: The owner of this object
         @type container: Bool
@@ -157,8 +156,26 @@ class NonLivingObjectData(BaseObjectData):
         @type gfx: String
         @param gfx: ???
         @returns: None"""
-        super(NonLivingObjectData, self).__init__(id, posx, posy, text, map, gfx, display)
+        super(NonLivingObjectData, self).__init__(ident, posx, posy, text, 
+                                                  cmap, gfx, display)
         self.container = container
         self.carryable = carryable
         self.owner = owner
+
+class MapDoor:
+    """A MapDoor is an item that when clicked transports the player to
+       another map"""
+    def __init__(self, name, new_map, location):
+        """@type name: string
+           @param name: name of fife object
+           @type new_map: string
+           @param new_map: name of new map
+           @type location: tuple
+           @param location: where to put the PC when map is loaded
+           @return: None"""
+        self.id = name
+        self.map = new_map
+        # location is an (int, int) which stores the intended location 
+        # of the PC on the new map
+        self.targ_coords = location
 
