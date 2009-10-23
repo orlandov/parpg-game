5358f7c53aa04e6c850b56e10a09bef86c754302
eliedebrauwer <eliedebrauwer@2475d057-1fa1-4ed2-bda5-76bff2e61f20>
2009-10-23 18:22:26 +0000

    Ticket #127. Fix by octavio, PEP-8 code cleanup, identation fix  fixes[s:trac, t:127]
    git-svn-id: http://parpg-svn.cvsdude.com/parpg/trunk/game@342 2475d057-1fa1-4ed2-bda5-76bff2e61f20

--- scripts/objects/__init__.py
+++ scripts/objects/__init__.py
@@ -27,27 +27,27 @@ def getAllObjects ():
     for module in object_modules:
         for class_name in module.__all__:
             result[class_name] = getattr (module,class_name)
-            
+
     return result
 
 def createObject(info, extra = {}):
-        """Called when we need to get an actual object. 
-           @type info: dict
-           @param info: stores information about the object we want to create
-           @type extra: dict
-           @param extra: stores additionally required attributes, like agent layer, engine etc.
-           @return: the object"""
-        # First, we try to get the type and ID, which every game_obj needs.
-        try:
-            obj_type = info.pop('type')
-            ID = info.pop('id')
-        except KeyError:
-            sys.stderr.write("Error: Game object missing type or id.")
-            sys.exit(False)
-        
-        # add the extra info
-        for key, val in extra.items():
-            info[key] = val
+    """Called when we need to get an actual object.
+    @type info: dict
+    @param info: stores information about the object we want to create
+    @type extra: dict
+    @param extra: stores additionally required attributes, like agent layer, engine etc.
+    @return: the object"""
+    # First, we try to get the type and ID, which every game_obj needs.
+    try:
+        obj_type = info.pop('type')
+        ID = info.pop('id')
+    except KeyError:
+        sys.stderr.write("Error: Game object missing type or id.")
+        sys.exit(False)
 
-        # this is for testing purposes
-        return getAllObjects()[obj_type](ID, **info)
+    # add the extra info
+    for key, val in extra.items():
+        info[key] = val
+
+    # this is for testing purposes
+    return getAllObjects()[obj_type](ID, **info)
